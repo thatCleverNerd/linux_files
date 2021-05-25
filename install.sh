8d35d9ae352e1b8e8c312df4cf84cc659c393364
richxvx <contrerasr101@gmail.com>
2021-05-25 13:35:06 -0700

    Added new files: install.sh and note.txt

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+border='================================'
+
+
+echo "$border"
+echo 'UPDATING'
+echo "$border"
+
+sudo apt update && sudo apt upgrade -y
+
+
+echo -e '\n\n\n\n\n'
+
+
+echo "$border"
+echo 'INSTALLING APPLICATIONS'
+echo "$border"
+
+sudo apt install git vim pdfsam atril -y
