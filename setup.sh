9c2da9f60d33344fa4ff7792d23b0a5033146475
Richard <contrerasr101@gmail.com>
2021-10-04 23:26:13 -0700

    Small changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 
 
 border='================================'
+thin_border="------------------------------------------------"
+
+
+if [ "$EUID" != 0 ]; then
+
+	echo -e "\n [!] Run script as root"
+	exit
+
+fi
 
 
 echo "$border"
@@ -12,10 +21,14 @@ sudo apt update && sudo apt upgrade -y
 
 
 echo -e '\n\n\n'
+echo -e "$thin_border"
 
 
 echo "$border"
 echo 'INSTALLING APPLICATIONS'
 echo "$border"
 
-sudo apt install git vim pdfsam atril python3 speedtest-cli -y
+sudo apt install git timeshift vim pdfsam atril python3 speedtest-cli -y
+
+
+sudo timeshift --create --comment "Creating initial backup from setup script"
