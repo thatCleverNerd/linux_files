24dd78fb742499da3ac8598af48003736024dc7d
thatclevernerd <contrerasr101@gmail.com>
2022-12-19 20:15:51 -0800

    Fixed some errors and added a new command to add my mpv.conf file as a symlink

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ echo "$border"
 echo -e "\t UPGRADING SYSTEM"
 echo "$border"
 
-sudo apt update && sudo apt upgrade -y
+#sudo apt update && sudo apt upgrade -y
 
 
 echo -e '\n\n\n'
@@ -22,14 +22,38 @@ echo "$border"
 echo -e "\t INSTALLING APPLICATIONS"
 echo "$border"
 
-sudo apt install git timeshift vim feh fzf yt-dlp curl htop pdfsam atril python3 speedtest-cli zsh com.github.phase1geo.annotator -y
+sudo apt install git timeshift vim feh fzf yt-dlp curl htop pdfsam atril python3 speedtest-cli zsh -y
+
+flatpak install flathub com.bitwarden.desktop -y
 
 echo "$border"
 echo -e "\t SETTING UP SYMLINKS"
 echo "$border"
 
-ln -s ~/linux_files/dotfiles/vimrc ~/.vimrc && echo -e "\n[+] vimrc symlnk created"
-sudo ln -s ~/linux_files/scripts/* /bin && echo -e "\n[+] All script symlnks created \n\n"
+ln -s ~/linux_files/dotfiles/vimrc ~/.vimrc
+echo -e "\n[+] vimrc symlnk created"
+
+sudo ln -s ~/linux_files/scripts/* /bin
+echo -e "\n[+] All script symlnks created \n\n"
+
+
+
+# checks if mpv folder exists
+
+mpvPath="/home/$USER/.config/mpv"
+
+if [[ -d $mpvPath ]]; then
+	ln -s ~/linux_files/dotfiles/mpv.conf ~/.config/mpv/
+	echo -e "\n\n [+] mpv.config symlink created \n"
+
+else
+	echo -e "\n\n [+] Creating mpv directory \n"
+	mkdir $mpvPath
+	ln -s ~/linux_files/dotfiles/mpv.conf ~/.config/mpv/
+	echo -e "\n\n [+] mpv.config symlink created \n"
+fi
+
+
 
 echo "$border"
 echo -e "\t SETTING UP ZSH"
@@ -46,5 +70,6 @@ echo -e "\n[+] Plugins have been setup"
 
 echo -e "\n Done :) (Dont't forget to reboot!) \n\n"
 
+check_reboot
 
 exit
