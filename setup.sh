ddaa60dd6519369faaf4cec680951535c5f551f1
thatclevernerd <contrerasr101@gmail.com>
2022-12-29 01:43:17 -0800

    Just a few updates

--- setup.sh
+++ setup.sh
@@ -4,14 +4,37 @@
 border='===================================================='
 thin_border="--------------------------------------------------"
 
-
 clear
+
+
+echo "$border"
+echo -e "\t UPDATING REPOs..."
+echo -e "$border\n\n"
+sudo apt update
+
+
+echo "$border"
+echo -e "\t SETTING UP ZSH"
+echo -e "$border\n\n"
+
+sudo apt install zsh -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm ~/.zshrc
+
+ln -s ~/linux_files/dotfiles/zshrc ~/.zshrc && echo -e "\n[+] zshrc symlink created \n\n"
+
+echo -e "[*] downloading zsh plugins \n\n" && git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+echo -e "\n[+] Plugins have been setup \n\n"
+
+
 
 echo "$border"
 echo -e "\t UPGRADING SYSTEM"
-echo "$border"
+echo -e "$border\n\n"
 
-sudo apt update && sudo apt upgrade -y
+sudo apt upgrade -y
 
 
 echo -e '\n\n\n'
@@ -20,22 +43,23 @@ echo -e "$thin_border"
 
 echo "$border"
 echo -e "\t INSTALLING APPLICATIONS"
-echo "$border"
+echo -e "$border\n\n"
 
-sudo apt install trilix git timeshift vim feh fzf yt-dlp curl htop pdfsam atril python3 speedtest-cli zsh -y
+sudo apt install tilix git timeshift vim feh fzf yt-dlp curl htop pdfsam atril python3 speedtest-cli -y
 
 flatpak install flathub trilium com.bitwarden.desktop -y
 
+
+
 echo "$border"
 echo -e "\t SETTING UP SYMLINKS"
-echo "$border"
+echo -e "$border\n\n"
 
 ln -s ~/linux_files/dotfiles/vimrc ~/.vimrc
-echo -e "\n[+] vimrc symlnk created"
+echo -e "\n [+] vimrc symlnk created"
 
 sudo ln -s ~/linux_files/scripts/* /bin
-echo -e "\n[+] All script symlnks created \n\n"
-
+echo -e "\n [+] All script symlnks created \n\n"
 
 
 # checks if mpv folder exists
@@ -54,22 +78,9 @@ else
 fi
 
 
-
-echo "$border"
-echo -e "\t SETTING UP ZSH"
-echo "$border"
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-rm ~/.zshrc
-
-ln -s ~/linux_files/dotfiles/zshrc ~/.zshrc && echo -e "\n[+] zshrc symlink created \n\n"
-
-echo -e "[*] downloading zsh plugins \n\n" && git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-echo -e "\n[+] Plugins have been setup"
-
 echo -e "\n Done :) (Dont't forget to reboot!) \n\n"
 
 check_reboot
+
 
 exit
