3ce3ed9432390caddc1c63561e32840d384f70b4
thatclevernerd <contrerasr101@gmail.com>
2026-05-08 18:52:57 -0700

    Change to steamdeck-setup.sh (removed check if running as root user. Using sudo in script for certain commands instead)

--- steamdeck-setup.sh
+++ steamdeck-setup.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-if [ $EUID -ne 0 ]; then
-	clear
-	echo -e "\n\n {!} Run Script as Root! \n\n"
-	exit
 
-fi
-
-steamos-devmode enable
-pacman -S tilix speedtest-cli --noconfirm
-
-flatpak install flathub net.agalwood.Motrix be.alexandervanhee.gradia com.brave.Browser com.valvesoftware.Steam.Utility.protontricks.Locale io.github.input_leap.input-leap net.davidotek.pupgui2 net.lutris.Lutris org.musicbrainz.Picard org.onlyoffice.desktopeditors org.remmina.Remmina org.strawberrymusicplayer.strawberry -y
+sudo steamos-devmode enable
+sudo pacman -S tilix --noconfirm
 
 
 dir="/home/deck/linux_files/scripts"
 
 count=0
 
+echo -e "\n\n\n"
+
 for file in "$dir"/*; do
 	if [ -f "$file" ]; then
 
-		ln -sf "$file" /bin/
+		sudo ln -sf "$file" /bin/
 		echo "[+] Created symlink for: $(basename "$file")"
 		((count++))
 	fi
@@ -29,3 +23,6 @@ done
 
 
 echo -e "\n [+] All script symlnks created \n\n"
+
+
+flatpak install flathub net.agalwood.Motrix be.alexandervanhee.gradia com.brave.Browser com.valvesoftware.Steam.Utility.protontricks.Locale io.github.input_leap.input-leap net.davidotek.pupgui2 net.lutris.Lutris org.musicbrainz.Picard org.onlyoffice.desktopeditors org.remmina.Remmina org.strawberrymusicplayer.strawberry -y
