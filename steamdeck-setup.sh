10903236a04b5fc3706921a74d5e3a11199e45df
thatclevernerd <contrerasr101@gmail.com>
2026-05-08 18:21:14 -0700

    Small changes to steamdeck-setup.sh and mp3rip

--- steamdeck-setup.sh
+++ steamdeck-setup.sh
@@ -12,8 +12,6 @@ pacman -S tilix speedtest-cli --noconfirm
 
 flatpak install flathub net.agalwood.Motrix be.alexandervanhee.gradia com.brave.Browser com.valvesoftware.Steam.Utility.protontricks.Locale io.github.input_leap.input-leap net.davidotek.pupgui2 net.lutris.Lutris org.musicbrainz.Picard org.onlyoffice.desktopeditors org.remmina.Remmina org.strawberrymusicplayer.strawberry -y
 
-curl "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-0.54.5.AppImage" -o ~/Desktop/AnyType.AppImage
-chmod +x ~/Desktop/AnyType.AppImage
 
 dir="/home/deck/linux_files/scripts"
 
